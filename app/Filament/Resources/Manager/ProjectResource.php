1e24975acb587c0d39de6ead2600c54b26a3337c
fauzanmuh <f4uz4n.oioi@gmail.com>
2024-09-18 11:47:19 +0700

    make crud project

---
+++ app/Filament/Resources/Manager/ProjectResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources\Manager;
+
+use App\Filament\Resources\Manager\ProjectResource\Pages;
+use App\Filament\Resources\Manager\ProjectResource\RelationManagers;
+use App\Models\Project;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProjectResource extends Resource
+{
+    protected static ?string $model = Project::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?int $navigationSort = 4;
+
+    public static function getNavigationLabel(): string
+    {
+        return 'Project';
+    }
+    public static function getPluralLabel(): string
+    {
+        return 'Project';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('project_name')
+                    ->required()
+                    ->label('Project Name'),
+                TextInput::make('project_client')
+                    ->required()
+                    ->label('Client Name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('project_name')
+                    ->label('Project Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('project_client')
+                    ->label('Client Name')
+                    ->searchable()
+                    ->sortable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjects::route('/'),
+            'create' => Pages\CreateProject::route('/create'),
+            'edit' => Pages\EditProject::route('/{record}/edit'),
+        ];
+    }
+}
