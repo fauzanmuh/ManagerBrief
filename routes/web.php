3728cbf186c65be4cb0b8381b082bdece9171131
fauzanmuh <f4uz4n.oioi@gmail.com>
2024-09-24 13:38:59 +0700

    update route pdf

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Filament\Resources\Developer\TaskReportResource;
+use Doctrine\DBAL\Logging\Middleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/user/developer/task-reports/download', [TaskReportResource::class, 'downloadPdf'])->name('task-reports.download');
+Route::get('/task-reports/download', [TaskReportResource::class, 'downloadPdf'])->name('task-reports.download')->middleware('auth');
